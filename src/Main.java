31b07ee565fefd7902db4f8a9c1674a83116feff
Kevin <nguyekev@uw.edu>
2022-10-08 15:00:02 -0700

    final

--- src/Main.java
+++ src/Main.java
@@ -1,67 +1,82 @@
-/**
- * Kevin Nguyen
- * TCSS 360A
- * Assignment 1: Minesweeper
- */
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-        Scanner input = new Scanner(new File("minesweeper_input.txt"));
-        int row  = input.nextInt();
-        int col = input.nextInt();
-        int fieldCase = 1;
-        while (row != 0 || col != 0){
-            char[][] field = new char[row][col];
-            for (int i = 0; i < row; i++){
-                String line = input.next();
-                for (int j = 0; j < col; j++){
-                    field[i][j] = line.charAt(j);
-                }
-            }
-            System.out.println("Field #" + fieldCase + ":");
-            for (int i = 0; i < row; i++){
-                for (int j = 0; j < col; j++){
-                    if (field[i][j] == '*'){
-                        System.out.print("*");
-                    } else {
-                        int count = 0;
-                        if (i > 0 && j > 0 && field[i-1][j-1] == '*'){
-                            count++;
-                        }
-                        if (i > 0 && field[i-1][j] == '*'){
-                            count++;
-                        }
-                        if (i > 0 && j < col-1 && field[i-1][j+1] == '*'){
-                            count++;
-                        }
-                        if (j > 0 && field[i][j-1] == '*'){
-                            count++;
-                        }
-                        if (j < col-1 && field[i][j+1] == '*'){
-                            count++;
-                        }
-                        if (i < row-1 && j > 0 && field[i+1][j-1] == '*'){
-                            count++;
-                        }
-                        if (i < row-1 && field[i+1][j] == '*'){
-                            count++;
-                        }
-                        if (i < row-1 && j < col-1 && field[i+1][j+1] == '*'){
-                            count++;
-                        }
-                        System.out.print(count);
+    public static void main(String[] theArgs){
+        Scanner input = new Scanner(System.in);
+        mineSweeper(input);
+    }
+
+    private static void mineSweeper(Scanner theInput) {
+        int row = theInput.nextInt();
+        int col = theInput.nextInt();
+        int fCase = 1;
+        while(!(row == 0 || col == 0)) {
+            theInput.nextLine();
+            int arr[][]= new int[row][col];
+            for(int i = 0 ; i < row; i++) {
+                char [] field = theInput.nextLine().toCharArray();
+                for(int j = 0; j < field.length; j++) {
+                    if(field[j] == '*') {
+                        adjacentAlert(arr, i, j);
                     }
                 }
-                System.out.println();
             }
-            row = input.nextInt();
-            col = input.nextInt();
-            if (row != 0 || col != 0){
+            fPrint(fCase, arr);
+
+            row = theInput.nextInt();
+            col = theInput.nextInt();
+            if(!(row == 0 || col == 0))
                 System.out.println();
+            fCase++;
+        }
+    }
+
+    private static void adjacentAlert(int arr[][], int a, int b) {
+        try {
+            arr[a][b] = -1;
+        } catch (ArrayIndexOutOfBoundsException e) {}
+        try {
+            if(!(arr[a-1][b-1] == -1))
+                arr[a-1][b-1]++;
+        } catch (ArrayIndexOutOfBoundsException e) {}
+        try {
+            if(!(arr[a-1][b] == -1))
+                arr[a-1][b]++;
+        } catch (ArrayIndexOutOfBoundsException e) {}
+        try {
+            if(!(arr[a-1][b+1] == -1))
+                arr[a-1][b+1]++;
+        } catch (ArrayIndexOutOfBoundsException e) {}
+        try {
+            if(!(arr[a][b-1] == -1))
+                arr[a][b-1]++;
+        } catch (ArrayIndexOutOfBoundsException e) {}
+        try {
+            if(!(arr[a][b+1] == -1))
+                arr[a][b+1]++;
+        } catch (ArrayIndexOutOfBoundsException e) {}
+        try {
+            if(!(arr[a+1][b-1] == -1))
+                arr[a+1][b-1]++;
+        } catch (ArrayIndexOutOfBoundsException e) {}
+        try {
+            if(!(arr[a+1][b] == -1))
+                arr[a+1][b]++;
+        } catch (ArrayIndexOutOfBoundsException e) {}
+        try {
+            if(!(arr[a+1][b+1] == -1))
+                arr[a+1][b+1]++;
+        } catch (ArrayIndexOutOfBoundsException e) {}
+    }
+    private static void fPrint(int fCase, int arr[][]) {
+        System.out.println("Field #"+ fCase +":");
+        for(int a = 0; a < arr.length;a++) {
+            for(int b = 0; b < arr[a].length; b++) {
+                if(arr[a][b] == -1)
+                    System.out.print("*");
+                else
+                    System.out.print(arr[a][b]);
             }
-            fieldCase++;
+            System.out.println();
         }
     }
 }
